2b5744197942f16310659d0830255c24bd1f111d
pth1209 <connorpoff@wireless-10-148-150-110.public.utexas.edu>
2024-01-27 12:59:57 -0600

    switched from local to aws s3

--- api/index.py
+++ api/index.py
@@ -8,35 +8,62 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+import boto3
 from unstructured.partition.auto import partition
 
-os.environ["OPENAI_API_KEY"] = "sk-<your key here>"
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["AWS_ACCESS_KEY_ID"] = ""
+os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+LOCAL_DOWNLOAD_PATH = 'downloads'
+os.makedirs(LOCAL_DOWNLOAD_PATH, exist_ok=True)
 
+BUCKET_NAME = 'defaultefhackathon'
+s3_client = boto3.client('s3')
 
 app = Flask(__name__)
 @app.route("/api/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
-    print(file)
-    #save file
-    file.save(os.path.join("uploads", file.filename))
+    if file:
+        # S3 path where file will be uploaded
+        file_path_in_s3 = f"{file.filename}"
 
-    return jsonify({"message": "File uploaded successfully"})
+        # Upload file to S3
+        s3_client.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file_path_in_s3
+        )
+        return jsonify({"message": "File uploaded successfully to S3"})
+
+    return jsonify({"message": "No file found"})
 
     
+
+def list_files_in_bucket(bucket_name):
+    """List files in an S3 bucket."""
+    files = s3_client.list_objects_v2(Bucket=bucket_name)
+    return [file['Key'] for file in files.get('Contents', [])]
+
+def download_file(bucket_name, s3_file_key, local_path):
+    """Download a file from S3 to a local path."""
+    s3_client.download_file(bucket_name, s3_file_key, local_path)
 
 @app.route("/api/chat")
 def chat():
     # get files from uploads folder
-    files = os.listdir("uploads")
-
-    # use unstructed to get the text from the files
+    files = list_files_in_bucket(BUCKET_NAME)
     texts = []
     text = ""
+    # Process each file
+    for s3_file_key in files:
+        local_file_path = os.path.join(LOCAL_DOWNLOAD_PATH, os.path.basename(s3_file_key))
+        download_file(BUCKET_NAME, s3_file_key, local_file_path)
+
+    # use unstructed to get the text from the files
+    files = os.listdir(LOCAL_DOWNLOAD_PATH)
     for file in files:
-        elements = partition(UPLOAD_FOLDER + "/" + file)
+        elements = partition(LOCAL_DOWNLOAD_PATH + "/" + file)
         for element in elements:
             text += str(element) + " "
         texts.append(text)
